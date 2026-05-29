1dd419afa6961efe1c6f26b31f12feb5d67880c4
Yogesh Kondurkar <kondurkaryogesh4@gmail.com>
2026-05-30 03:19:05 +0530

    updated the vite config

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // Set base to "/repo-name/" for a project site (username.github.io/repo-name)
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio",
+  base: "/portfolio/",
 });
